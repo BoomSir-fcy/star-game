48a61339906e57d911989f1b8e0404926aea6b8e
kksam <1011847054@qq.com>
2022-03-29 13:50:46 +0800

    fix:弹框布局

--- src/views/Star/components/Modal/index.ts
+++ src/views/Star/components/Modal/index.ts
@@ -2,4 +2,5 @@ export * from './RechargeAssets';
 export * from './ThingRepairModal';
 export * from './ThingDestoryModal';
 export * from './ThingUpgradesModal';
+export * from './ThingHpModal';
 export * from './TextList';
