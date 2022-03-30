9060c3a9da13d242a0b7a3dafde5ef8c8c45f01d
BoomSir-fcy <351984524@qq.com>
2022-03-30 10:56:02 +0800

    feat:充提

--- src/apis/balance/balance.api.ts
+++ src/apis/balance/balance.api.ts
@@ -6,4 +6,10 @@ export class BalanceApi extends Http {
     const res = await this.get('balance/list');
     return res;
   }
+
+  // 提币
+  async withdraw(params: Api.Balance.DrawbalanceSignMessage) {
+    const res = await this.post('balance/withdraw', params);
+    return res;
+  }
 }
--- src/utils/addressHelpers.ts
+++ src/utils/addressHelpers.ts
@@ -1,26 +1,26 @@
 // import { ChainId } from 'dsgswap-sdk'
-import { ChainId, } from 'config/wallet/config';
+import { ChainId } from 'config/wallet/config';
 
-import addresses from 'config/constants/contracts'
+import addresses from 'config/constants/contracts';
 
 type Address = {
   [chain in ChainId]?: string;
 };
 
 export const getAddress = (address: Partial<Address>): string => {
-  const chainId: ChainId = process.env.REACT_APP_CHAIN_ID as unknown as ChainId
+  const chainId: ChainId = process.env.REACT_APP_CHAIN_ID as unknown as ChainId;
   if (address[chainId]) {
     return address[chainId] as string;
   }
-  return address[ChainId.MAINNET] as string
-}
+  return address[ChainId.MAINNET] as string;
+};
 
 export const getMulticallAddress = () => {
-  return getAddress(addresses.multiCall)
-}
+  return getAddress(addresses.multiCall);
+};
 export const getWEtherAddress = () => {
-  return getAddress(addresses.WBNB)
-}
+  return getAddress(addresses.WBNB);
+};
 
 export const getDsgAddress = () => {
   return getAddress(addresses.DsgToken);
@@ -35,21 +35,25 @@ export const getMatterAddress = () => {
 };
 
 export const getUserAgentAddress = () => {
-  return getAddress(addresses.UserAgent)
-}
+  return getAddress(addresses.UserAgent);
+};
 
 export const getMysteryBoxAddress = () => {
-  return getAddress(addresses.MysteryBox)
-}
+  return getAddress(addresses.MysteryBox);
+};
 
 export const getGalaxyAddress = () => {
-  return getAddress(addresses.Galaxy)
-}
+  return getAddress(addresses.Galaxy);
+};
 
 export const getPlanetAddress = () => {
-  return getAddress(addresses.Planet)
-}
+  return getAddress(addresses.Planet);
+};
 
 export const getUserVaultAddress = () => {
-  return getAddress(addresses.UserVault)
-}
+  return getAddress(addresses.UserVault);
+};
+
+export const getBoxAddress = () => {
+  return getAddress(addresses.BoxToken);
+};
