e4cf6b199616c35f7a7068cc8093c3db4cbc00d8
BoomSir-fcy <351984524@qq.com>
2022-04-15 09:57:17 +0800

    feat:注册查询注册结果更改用户状态

--- src/apis/httpErrorEvent.ts
+++ src/apis/httpErrorEvent.ts
@@ -1,6 +1,8 @@
 import eventBus from '../utils/eventBus';
+import { ResponseCode } from './type';
 
 const dispatchHttpErrorEvent = (data: Api.Error) => {
+  if (data?.code === ResponseCode.NOTFOUNTUSER) return;
   if (data?.code !== 0) {
     eventBus.dispatchEvent(
       new MessageEvent('httpError', {
