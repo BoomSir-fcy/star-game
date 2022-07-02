9bf52c66159c0d277fe7ddb9a69a356f458a04d9
BoomSir-fcy <351984524@qq.com>
2022-07-02 11:27:10 +0800

    feat:联盟消息时间选择器改为日期范围选择

--- src/views/BattleReport/BattleTop.tsx
+++ src/views/BattleReport/BattleTop.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useMemo } from 'react';
 import styled from 'styled-components';
 import {
   Card,
@@ -14,8 +14,9 @@ import { useTranslation } from 'contexts/Localization';
 import { useDispatch } from 'react-redux';
 import 'react-dates/initialize';
 import 'react-dates/lib/css/_datepicker.css';
-import { SingleDatePicker } from 'react-dates';
+import { DateRangePicker } from 'react-dates';
 import dayjs from 'dayjs';
+import moment from 'moment';
 
 const TitleBox = styled(Flex)`
   width: 512px;
@@ -27,7 +28,6 @@ const TitleBox = styled(Flex)`
 `;
 
 const RecordBox = styled(Flex)`
-  width: 316px;
   height: 90px;
   background: url('/images/battleReport/infoBg.png') no-repeat;
   background-size: 100% 100%;
@@ -35,14 +35,14 @@ const RecordBox = styled(Flex)`
   justify-content: space-between;
   padding: 0 34px 0 45px;
   margin-left: 20px;
-  .SingleDatePickerInput__withBorder {
-    border: none;
-  }
-  .SingleDatePickerInput {
+  .DateRangePickerInput {
+    margin-top: 10px;
     background-color: transparent;
+    display: flex;
+    align-items: center;
     .DateInput {
       background: transparent;
-      #date_input {
+      .DateInput_input {
         background-color: transparent;
         text-shadow: 0px 3px 0.4em #79c6c4;
         background: linear-gradient(
@@ -58,7 +58,6 @@ const RecordBox = styled(Flex)`
         font-family: SourceHanSansCN-Bold;
         line-height: 1.5;
         /* margin-left: 20px; */
-        margin-top: 10px;
         font-size: 22px;
         font-style: italic;
         padding: 0;
@@ -69,9 +68,17 @@ const RecordBox = styled(Flex)`
     .CalendarDay {
       line-height: 38px;
     }
-    .SingleDatePicker_picker__directionLeft {
-      top: 64px !important;
+    .DateRangePickerInput_arrow {
+      color: #79c6c4;
+      .DateRangePickerInput_arrow_svg {
+        fill: #79c6c4;
+        width: 60px;
+        height: 34px;
+      }
     }
+    /* .SingleDatePicker_picker__directionLeft {
+      top: 64px !important;
+    } */
   }
 `;
 
@@ -89,21 +96,37 @@ interface contInfo {
 
 export const BattleTop: React.FC<{
   cont: contInfo;
-  upDate: (e) => void;
-}> = ({ cont, upDate }) => {
+  Start_time: number;
+  End_time: number;
+  setStart_time: (e) => void;
+  setEnd_time: (e) => void;
+}> = ({ cont, Start_time, End_time, setStart_time, setEnd_time }) => {
   const { t } = useTranslation();
   const dispatch = useDispatch();
 
-  const [date, setDate] = useState();
-  const [isFocused, setIsFocused] = useState(false);
+  const initialStartDate = useMemo(() => {
+    return moment(Start_time * 1000);
+  }, [Start_time]);
+
+  const initialEndDate = useMemo(() => {
+    return moment(End_time * 1000);
+  }, [End_time]);
+
+  const [isFocused, setIsFocused] = useState(null);
 
   const onDateChange = (e: any) => {
-    setDate(e);
-    const time = dayjs(e).unix();
-    upDate(time);
+    const Starttime = moment(e.startDate).format('YYYY-MM-DD');
+    const Endtime = moment(e.endDate).format('YYYY-MM-DD');
+    const Start =
+      new Date(new Date(Starttime).toLocaleDateString()).getTime() / 1000;
+    const End =
+      new Date(new Date(Endtime).toLocaleDateString()).getTime() / 1000;
+    setStart_time(Start);
+    setEnd_time(End);
+    setIsFocused(null);
   };
-  const onFocusChange = ({ focused }) => {
-    setIsFocused(focused);
+  const onFocusChange = e => {
+    setIsFocused(e);
   };
 
   return (
@@ -113,7 +136,7 @@ export const BattleTop: React.FC<{
         <RefreshButton
           ml='33px'
           onRefresh={() => {
-            upDate(new Date(new Date().toLocaleDateString()).getTime() / 1000);
+            // upDate(new Date(new Date().toLocaleDateString()).getTime() / 1000);
           }}
         />
       </Box>
@@ -122,7 +145,7 @@ export const BattleTop: React.FC<{
           {t('Battle details')}
         </MarkText>
       </TitleBox>
-      <RecordBox>
+      <RecordBox width='316px'>
         <MarkText ml='20px' mt='10px' fontSize='22px' bold fontStyle='italic'>
           {t('Total number of battles')}
         </MarkText>
@@ -138,14 +161,8 @@ export const BattleTop: React.FC<{
           </MarkText>
         </Flex>
       </RecordBox>
-      <RecordBox>
-        {/* <MarkText ml='20px' mt='10px' fontSize='22px' bold fontStyle='italic'>
-          {t('Victories')}
-        </MarkText>
-        <MarkText fontSize='28px' bold fontStyle='italic'>
-          {cont.WinCont}
-        </MarkText> */}
-        <SingleDatePicker
+      <RecordBox width='550px'>
+        {/* <SingleDatePicker
           id='date_input'
           date={date}
           focused={isFocused}
@@ -156,26 +173,34 @@ export const BattleTop: React.FC<{
           placeholder={dayjs().format('MM/DD/YYYY')}
           readOnly
           maxDate={new Date(new Date().toLocaleDateString()).getTime() / 1000}
+        /> */}
+        <DateRangePicker
+          readOnly
+          startDate={initialStartDate}
+          endDate={initialEndDate}
+          focusedInput={isFocused}
+          onFocusChange={onFocusChange}
+          onDatesChange={onDateChange}
+          maxDate={moment(new Date(new Date().toLocaleDateString()).getTime())}
+          // disabled='endDate'
+          numberOfMonths={1}
+          isOutsideRange={() => {}}
+          startDateId='_startDate'
+          endDateId='_endDateId'
+          noBorder
         />
         <Flex
-          onClick={() => setIsFocused(true)}
+          onClick={() => setIsFocused('startDate')}
           justifyContent='center'
           alignItems='center'
-          width='60px'
+          width='110px'
           height='60px'
           position='relative'
+          mr='20px'
         >
           <DownImg src='/images/commons/icon/back.png' />
         </Flex>
       </RecordBox>
-      {/* <RecordBox>
-        <MarkText ml='20px' mt='10px' fontSize='22px' bold fontStyle='italic'>
-          {t('Number of failed games')}
-        </MarkText>
-        <MarkText fontSize='28px' bold fontStyle='italic'>
-          {cont.FailedCont}
-        </MarkText>
-      </RecordBox> */}
     </Flex>
   );
 };
